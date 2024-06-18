d15d6b0c1fcc9c1f37c8d499def294a0625cce4e
dohoang nam <do.hoangnam9x@gmail.com>
2024-06-18 18:16:50 +0700

    move analytic to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import DefaultLayout from "@/components/_layout/DefaultLayout.vue";
+import { inject } from "@vercel/analytics";
+
+const isProd = import.meta.env.PROD;
+
+inject({
+  mode: isProd ? "production" : "development",
+  framework: "vue",
+  debug: !isProd,
+});
 </script>
 
 <template>
